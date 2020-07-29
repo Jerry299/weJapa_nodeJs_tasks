86a4a30e07b5a9d36c6c586c7701a4300bf184db
Jerry299 <josephjeremiah564@yahoo.com>
2020-07-29 04:18:22 +0100

    task2, repl task

---
+++ replApp.js
@@ -0,0 +1,26 @@
+// build a basic REPL server that takes a command and prints it in
+//     uppercase if the command is lowercase and lowercase if the command is uppercase using Node.
+
+// my solution
+
+const repl = require("repl");
+
+const message = "Welcome to repl";
+
+// repl.start("> ").context.welcome = message;
+const convertCase = (cmd) => {
+  // check uppercase
+  if (cmd === cmd.toUpperCase()) {
+    console.log(cmd.toLowerCase());
+  }
+  //check lower case
+  else if (cmd === cmd.toLowerCase()) {
+    console.log(cmd.toUpperCase());
+  } // else tell the user that the system cannot recognise their input
+  else {
+    console.log(
+      "Input not recognised,try a capitalized input or a small letter input"
+    );
+  }
+};
+repl.start({ prompt: "> ", eval: convertCase });
