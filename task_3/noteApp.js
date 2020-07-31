71211e76697865c2af37f06076d622ea684034f3
Jerry299 <josephjeremiah564@yahoo.com>
2020-07-31 21:34:15 +0100

    business note dir up

--- task_3/noteApp.js
+++ task_3/noteApp.js
@@ -2,9 +2,33 @@ const fs = require("fs");
 const http = require("http");
 const { parse } = require("querystring");
 
+//beginning of adding a business note endpoint
 const server = http.createServer((request, response) => {
-  // taking a business note
-  if (request.method === "POST" && request.url === "/notes/business") {
+  // creating a read note endpoint for business A GET REQUEST
+  if (request.url === "/notes/read/business" && request.method === "GET") {
+    fs.readFile("./businessNotes.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log(
+          "There was an error reading the file in business Notes:   ",
+          err
+        );
+        response.end("Could not read note");
+      }
+      // response.end({ notes: data });
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify({ note: data.toString() }));
+    });
+  }
+  // create an instruction endpoint
+  else if (request.url === "/" && request.method === "GET") {
+    const instructions = `to add a new business note, go to this endpoint : '/notes/business', To read the content of the business note
+    note go to this endpoint : '/notes/read/business'`;
+
+    response.end(JSON.stringify({ instructions }));
+  }
+
+  // taking a business note A POST REQUEST
+  else if (request.method === "POST" && request.url === "/notes/business") {
     let body = "";
     request.on("error", (err) => {
       console.log(err);
@@ -24,24 +48,19 @@ const server = http.createServer((request, response) => {
       //append a file to business note.txt and read it out
       // also make sure the note is not empty
 
-      if (body.length > 10) {
-        fs.appendFile("./businessNotes.txt", body.note + "\n \n", (err) => {
-          if (err) {
-            console.log(
-              "There was an error writing to the businessNote   :  ",
-              err
-            );
-          }
-          fs.readFile("./businessNotes.txt", "utf8", (err, data) => {
+      if (body.note.length > 10) {
+        fs.appendFile(
+          "./businessNotes.txt",
+          body.note.padStart(3, " "),
+          (err) => {
             if (err) {
               console.log(
-                "There was an error reading the file in business Notes:   ",
+                "There was an error writing to the businessNote   :  ",
                 err
               );
             }
-            console.log(data);
-          });
-        });
+          }
+        );
       } else {
         response.statusCode = 400;
         response.end(
@@ -51,33 +70,11 @@ const server = http.createServer((request, response) => {
 
       response.end("Note has been succesfuly added to business note");
     });
-  } else {
-    response.statusCode = 404;
-    response.end("Enter a business note in the note");
   }
 });
+//end of adding a business note endpoint
 
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
-
-//append a file to business note.txt and read it out
-
-//   fs.appendFile("./businessNotes.txt", body, (err) => {
-//     if (err) {
-//       console.log(
-//         "There was an error writing to the businessNote   :  ",
-//         err
-//       );
-//     }
-//     fs.readFile("./businessNotes.txt", "utf8", (err, data) => {
-//       if (err) {
-//         console.log(
-//           "There was an error reading the file in business Notes:   ",
-//           err
-//         );
-//       }
-//       console.log(data);
-//     });
-//   })
