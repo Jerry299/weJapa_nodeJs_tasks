8c4c74ff621a54f9f6ae800a1e2c21ba2681f3ab
Jerry299 <josephjeremiah564@yahoo.com>
2020-08-24 21:50:14 +0100

    made changes to map

--- task_5/routes/city.js
+++ task_5/routes/city.js
@@ -10,19 +10,30 @@ router.get("/map", (req, res) => {
 });
 router.get("/result", (req, res) => {
   let city = req.query.city;
-  console.log(city, " ==== city");
+
   axios
     .get(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=37068910d3019fcd4a44b0e539978cec`
     )
     .then((response) => {
-      console.log(response.data);
       res.render("weather", { data: response.data });
     })
     .catch((err) => {
       console.log(err);
       res.status(404).render("cityError");
     });
+});
+router.get("/map-result", (req, res) => {
+  const map = req.query.map;
+  const longitude = Number(req.query.long);
+  const latitude = Number(req.query.lat);
+  let reg = /\d/g;
+  const api =
+    "https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}";
+  //validate input
+  if (isNaN(longitude)) {
+    res.json({ msg: "matched" });
+  }
 });
 
 export default router;
