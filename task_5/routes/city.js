fa739452f3727de1d835b4c2f63c4f4116e07050
Jerry299 <josephjeremiah564@yahoo.com>
2020-08-25 06:27:58 +0100

    adding map

--- task_5/routes/city.js
+++ task_5/routes/city.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
 import axios from "axios";
+import key from "../config";
+import L from "leaflet";
 
 router.get("/home", (req, res) => {
   res.render("home");
@@ -13,7 +15,7 @@ router.get("/result", (req, res) => {
 
   axios
     .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=37068910d3019fcd4a44b0e539978cec`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
     )
     .then((response) => {
       res.render("weather", { data: response.data });
@@ -25,15 +27,26 @@ router.get("/result", (req, res) => {
 });
 router.get("/map-result", (req, res) => {
   const map = req.query.map;
-  const longitude = Number(req.query.long);
-  const latitude = Number(req.query.lat);
-  let reg = /\d/g;
-  const api =
-    "https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}";
-  //validate input
-  if (isNaN(longitude)) {
-    res.json({ msg: "matched" });
-  }
+  const longitude = req.query.long;
+  const latitude = req.query.lat;
+  let zoom = 6;
+
+  // const api = `https://tile.openweathermap.org/map/precipitation_new/6/6.7/6.1.png?appid=37068910d3019fcd4a44b0e539978cec`;
+  // axios
+  //   .get(
+  //     `https://tile.openweathermap.org/map/${map}/${zoom}/${longitude}/${latitude}.png?appid=${key}`
+  //   )
+  //   .then((response) => {
+  //     console.log(response.data);
+  //     res.status(200).render("mapsResult", { data: response.data });
+  //   })
+  //   .catch((err) => {
+  //     console.log("map Error", err);
+  //     res.status(404).render("cityError");
+  //   });
+
+  const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+  res.render("mapsResult", { data: mymap });
 });
 
 export default router;
